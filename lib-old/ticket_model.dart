6ebdf3ff74ab03c96ecd2a25210e0de5acfc97b8
vinothini <vinothini9396@gmail.com>
2023-11-27 15:06:51 +0530

    thiran task 3

--- lib-old/ticket_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:geolocator/geolocator.dart';
-
-class Ticket {
-  final String id;
-  final String title;
-  final String description;
-
-  Ticket ({required this.id,required this.title,required this.description});
-}
